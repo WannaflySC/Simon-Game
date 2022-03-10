ec759a65f28a748c1bbf53fe6e73842db862451e
Sihang Cheng <sche273@uwo.ca>
2022-03-10 14:47:37 -0500

    Simon Game
    Simon Game developed with JQuery

---
+++ game.js
@@ -0,0 +1,94 @@
+var userClickedPattern = [];
+
+var gamePattern = [];
+
+var started = false;
+
+var level = 0;
+
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+
+$(document).keypress(function(){
+  if (!started){
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+
+});
+
+$(".btn").click(function(){
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+  animatePress(userChosenColor);
+  playSound(userChosenColor);
+  checkAnswer(userClickedPattern.length-1);
+});
+
+
+//1. Create a new function called checkAnswer(), it should take one input with the name currentLevel
+function checkAnswer(currentLevel) {
+
+    //3. Write an if statement inside checkAnswer() to check if the most recent user answer is the same as the game pattern. If so then log "success", otherwise log "wrong".
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+      console.log("success");
+
+      //4. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
+      if (userClickedPattern.length === gamePattern.length){
+
+        //5. Call nextSequence() after a 1000 millisecond delay.
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+
+      }
+
+    } else {
+
+      console.log("wrong");
+      playSound("wrong");
+      $("body").addClass("game-over");
+      setTimeout(function(){
+          $("body").removeClass("game-over");
+      }, 200);
+      $("#level-title").text("Game Over, Press Any Key To Restart");
+      startOver();
+    }
+
+}
+
+function nextSequence(){
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor((Math.random() * 4));
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColor);
+}
+
+function playSound(name){
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor){
+  $("#" + currentColor).click(function(){
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function(){
+            $(".btn").removeClass("pressed");
+    }, 100);
+  });
+}
+
+
+function startOver(){
+  gamePattern = [];
+
+  started = false;
+
+  level = 0;
+}
